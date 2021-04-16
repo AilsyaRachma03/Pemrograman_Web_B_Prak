8f76f3e17e940b83b5d3ff39995a55936f673a68
AilsyaRachma03 <63080602+AilsyaRachma03@users.noreply.github.com>
2021-04-16 22:49:44 +0700

    yuk bisa yuk

---
+++ Pert_5/why/config.php
@@ -0,0 +1,12 @@
+<?php
+     
+    // define('BASE_URL', 'http://localhost:2000/Belajar PHP/PDO');
+
+    // db
+    define('DB_HOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'web_praktikum');
+
+
+?>
